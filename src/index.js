4689f09843745337095baf770b564a565dbf6b3a
mnauliady <mnauliady@gmail.com>
2023-02-23 10:17:33 +0700

    hooks useEffect time

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Hook menampilkan jam
+function Clock() {
+  const [date, setDate] = useState(new Date());
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // use effect hook
+  useEffect(() => {
+    setInterval(() => setDate(new Date()), 1000);
+  });
+
+  return (
+    <div>
+      <h1>{date.toLocaleTimeString()}</h1>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Clock />);
